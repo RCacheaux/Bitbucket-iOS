ffea4a4ba77d8ebd90de7184aefad55916d00717
René Cacheaux <rcacheaux@atlassian.com>
2016-03-03 11:14:00 -0600

    First working version.

---
+++ Bitbucket/Sources/iOS/Screens/Account/AccountViewController.swift
@@ -0,0 +1,49 @@
+//  Copyright © 2016 Atlassian Pty Ltd. All rights reserved.
+
+import UIKit
+import BitbucketKit
+import OAuthKit
+
+class AccountViewController: UITableViewController {
+  @IBOutlet weak var displayNameCell: UITableViewCell!
+  @IBOutlet weak var usernameCell: UITableViewCell!
+  var getUserAction: GetAuthenticatedUserAction?
+  var accountStore: AccountStore!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.refreshControl?.addTarget(self, action: "reloadData", forControlEvents: UIControlEvents.ValueChanged)
+  }
+
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    reloadData()
+  }
+
+  func reloadData() {
+    self.refreshControl?.beginRefreshing()
+    let getUserAction = GetAuthenticatedUserAction(accountStore: accountStore)
+    getUserAction.completionBlock = {
+      switch getUserAction.outcome {
+      case .Success(let user):
+        dispatch_async(dispatch_get_main_queue()) {
+          self.render(user)
+          self.refreshControl?.endRefreshing()
+        }
+      default:
+        print("Something went wrong trying to get user's account.")
+      }
+    }
+    getUserAction.start()
+    self.getUserAction = getUserAction
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+
+  func render(user: User) {
+    displayNameCell.textLabel?.text = user.displayName
+    usernameCell.detailTextLabel?.text = user.username
+  }
+}
